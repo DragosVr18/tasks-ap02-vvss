1dd4c58737002b4ae65fb00ffc2f743be0489bd6
DragosVr18 <vacarasudragosstefan145@gmail.com>
2025-03-27 22:44:26 +0200

    Tests for DateService, added annotations

--- src/test/java/tasks/services/DateServiceTest.java
+++ src/test/java/tasks/services/DateServiceTest.java
@@ -1,13 +1,20 @@
 package tasks.services;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import tasks.model.ArrayTaskList;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("DateService tests")
+@Tag("DateService")
 class DateServiceTest {
     private final DateService dateService = new DateService(new TasksService(new ArrayTaskList()));
 
@@ -33,6 +40,7 @@ class DateServiceTest {
         }
     }
 
+    @DisplayName("Equivalence class test cases for getDateMergedWithTime")
     @ParameterizedTest
     @CsvSource({
             "10:40, true",
@@ -50,46 +58,51 @@ class DateServiceTest {
         testGetDateMergedWithTime(testTime, isValid);
     }
 
+    @DisplayName("BVA test cases for getDateMergedWithTime")
     @ParameterizedTest
-    @CsvSource({
-            "00:00, true",
-            "00:-1, false",
-            "00:01, true",
-            "00:59, true",
-            "00:58, true",
-            "00:60, false",
-            "-1:00, false",
-            "-1:-1, false",
-            "-1:01, false",
-            "-1:59, false",
-            "-1:58, false",
-            "-1:60, false",
-            "01:00, true",
-            "01:-1, false",
-            "01:01, true",
-            "01:59, true",
-            "01:58, true",
-            "01:60, false",
-            "23:00, true",
-            "23:-1, false",
-            "23:01, true",
-            "23:59, true",
-            "23:58, true",
-            "23:60, false",
-            "22:00, true",
-            "22:-1, false",
-            "22:01, true",
-            "22:59, true",
-            "22:58, true",
-            "22:60, false",
-            "24:00, false",
-            "24:-1, false",
-            "24:01, false",
-            "24:59, false",
-            "24:58, false",
-            "24:60, false"
-    })
+    @MethodSource("boundaryValueTestCases")
     void getDateMergedWithTimeValidBVA(String testTime, boolean isValid) {
         testGetDateMergedWithTime(testTime, isValid);
+    }
+
+    private static Stream<Arguments> boundaryValueTestCases() {
+        return Stream.of(
+                Arguments.of("00:00", true),
+                Arguments.of("00:-1", false),
+                Arguments.of("00:01", true),
+                Arguments.of("00:59", true),
+                Arguments.of("00:58", true),
+                Arguments.of("00:60", false),
+                Arguments.of("-1:00", false),
+                Arguments.of("-1:-1", false),
+                Arguments.of("-1:01", false),
+                Arguments.of("-1:59", false),
+                Arguments.of("-1:58", false),
+                Arguments.of("-1:60", false),
+                Arguments.of("01:00", true),
+                Arguments.of("01:-1", false),
+                Arguments.of("01:01", true),
+                Arguments.of("01:59", true),
+                Arguments.of("01:58", true),
+                Arguments.of("01:60", false),
+                Arguments.of("23:00", true),
+                Arguments.of("23:-1", false),
+                Arguments.of("23:01", true),
+                Arguments.of("23:59", true),
+                Arguments.of("23:58", true),
+                Arguments.of("23:60", false),
+                Arguments.of("22:00", true),
+                Arguments.of("22:-1", false),
+                Arguments.of("22:01", true),
+                Arguments.of("22:59", true),
+                Arguments.of("22:58", true),
+                Arguments.of("22:60", false),
+                Arguments.of("24:00", false),
+                Arguments.of("24:-1", false),
+                Arguments.of("24:01", false),
+                Arguments.of("24:59", false),
+                Arguments.of("24:58", false),
+                Arguments.of("24:60", false)
+        );
     }
 }
